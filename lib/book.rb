e6b4be3763b7763945daa00a21465ce545b89f2c
Lee McCormick <lzm0010@gmail.com>
2020-01-29 18:44:13 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,14 @@
 class Book
   
   attr_accessor :title, :author
+  attr_reader
   def initialize(title)
     @title = title
   end
   
   def page_count=(num)
-    @page_count
+    @page_count = num
   end
+
   
 end
